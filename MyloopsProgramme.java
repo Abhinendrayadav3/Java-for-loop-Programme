3ee3679c78b4732a355f9b81ddde95153e25f83e
Abhinendra Yadav <75690933+Abhinendrayadav3@users.noreply.github.com>
2020-12-19 14:49:37 +0530

    Add files via upload

---
+++ MyloopsProgramme.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public class MyloopsProgramme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int i = 0; 
+				while (i<=10)
+			
+		{
+			System.out.println("hello world");
+			i++;
+		}
+
+	}
+
+}
