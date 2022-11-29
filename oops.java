d34a492c68cc46232ab76d94dbb6c5ed214a085a
Abhinendra Singh <abhinendrayadav3@gmail.com>
2022-11-29 21:51:18 +0530

    Update oops.java

--- oops.java
+++ oops.java
@@ -1,36 +1,49 @@
 
-class car{
- String type; //electric,diesel,petrol
- String brand;
- public void info(){
-    System.out.println(this.type);
-    System.out.println(this.brand);}
+// class car{
+//  String type; //electric,diesel,petrol
+//  String brand;
+//  public void info(){
+//     System.out.println(this.type);
+//     System.out.println(this.brand);}
 
- public void info2(){
-    System.out.println(this.type);
-    System.out.println(this.brand);
- }
- car(car car2){
-    this.type=car2.type;
-    this.brand=car2.brand;
+//  public void info2(){
+//     System.out.println(this.type);
+//     System.out.println(this.brand);
+//  }
+//  car(car car2){
+//     this.type=car2.type;
+//     this.brand=car2.brand;
 
- }
- car(){
+//  }
+//  car(){
 
- }
-}
+//  }
+// }
 
-public class oops {
+// public class oops {
   
+//     public static void main(String[] args) {
+//         car car1=new car();
+//         car1.type="diesel";
+//         car1.brand ="GLA";
+        
+  
+//         car car2=new car(car1);
+        
+//          car2.info2();
+//     }
+
+// }
+
+
+//------------inheritance--------------//
+
+
+
+
+
+public class oops{
     public static void main(String[] args) {
-        car car1=new car();
-        car1.type="diesel";
-        car1.brand="GLA";
         
-  
-        car car2=new car(car1);
-        
-         car2.info2();
     }
-
 }
