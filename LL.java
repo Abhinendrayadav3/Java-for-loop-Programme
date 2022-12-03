ea75f5d0fe59ae1e394de58eb0e4e6b790aa1c8e
Abhinendra Singh <abhinendrayadav3@gmail.com>
2022-12-03 23:50:16 +0530

    Update LL.java

--- LL.java
+++ LL.java
@@ -11,13 +11,30 @@ class LL{
 
     //operations performed in linkedList
     //add operation-first,last
+
+    //add operation on first pos
     public void addFirst(String data){
-        Node newNode=
-        if (head==null)
-        return;
+        Node newNode= new Node(data); //this is for making the new node
+        if (head==null) // if new node is null then we have to do nothing
+        {
+          head=newNode;
+          return;
+        }
+        else if (head!=null){
+            newNode.next=head;
+            head=newNode;
+
+        }
+    }
+    //add element at the last pos
+    public void addLast(String data){
 
     }
+
+
     public static void main(String[] args) {
         LL list=new LL();
+        list.addFirst('a');
+        list.addFirst('is')
     }
 }
