3a97336441ea348c86283c2db466249a09ea473f
Abhinendra Singh <abhinendrayadav3@gmail.com>
2022-12-03 17:40:03 +0530

    Create LL.java

---
+++ LL.java
@@ -0,0 +1,23 @@
+class LL{
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    //operations performed in linkedList
+    //add operation-first,last
+    public void addFirst(String data){
+        Node newNode=
+        if (head==null)
+        return;
+
+    }
+    public static void main(String[] args) {
+        LL list=new LL();
+    }
+}
