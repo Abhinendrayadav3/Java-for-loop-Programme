2cae41f81d1d2df5fcca7817d6ad8d1103a5d925
Abhinendra Singh <abhinendrayadav3@gmail.com>
2022-12-07 00:39:39 +0530

    required changes

--- sample.java
+++ sample.java
@@ -1,18 +1,11 @@
-class dog{
-    String breed;
-    String gender;
-    public void info(){
-        System.out.println(this.breed);
-        System.out.println(this.gender);
-    }
+import java.sql.Array;
+import java.util.*;
+class sample{
+
+public static void main(String[] args) {
+    LinkedList<Integer> ll =new LinkedList<>();
+    ll.add(0);
+    ll.add(0,6);
+    System.out.println(ll);
 }
-
-
-public class sample {
-  public static void main(String[] args) {
-        dog animal1=new dog();
-        animal1.breed="labra dog";
-        animal1.gender="male";
-        animal1.info();
-    }
 }
