b3bbc346d3c439c2eecb619827707a672703db2e
Abhinendra Singh <abhinendrayadav3@gmail.com>
2023-01-05 14:53:33 +0530

    basic parsing commit

---
+++ parsing.java
@@ -0,0 +1,8 @@
+public class parsing{
+    public static void main(String [] args){
+        String s="Hello parsing";
+        int Integer.parseInt("1234");
+        System.out.println(s);
+
+    }
+}
