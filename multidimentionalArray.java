755126b52f10a104e464d6c4c4b9013943fed9ef
Abhinendra Singh <abhinendrayadav3@gmail.com>
2023-01-05 16:19:01 +0530

    .

--- multidimentionalArray.java
+++ multidimentionalArray.java
@@ -1,8 +1,10 @@
 public class multidimentionalArray {
     public static void main(String[] args) {
         int mullu[][]={{1,2,3,4},{8,3,9,2,1}};
-        for(int i:mullu){
-            System.out.println(mullu[i]);
+        for(int i=0;i<mullu.length;i++){
+            for(int j=0;j<mullu[i].length;j++){
+                System.out.println(mullu[i][j]);
+            }
         }
     }
 }
