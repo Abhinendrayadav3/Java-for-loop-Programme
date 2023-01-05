f6d94ba6ed0bde8e6edd234f13d59f6f233f6a88
Abhinendra Singh <abhinendrayadav3@gmail.com>
2023-01-05 16:17:46 +0530

    .

---
+++ multidimentionalArray.java
@@ -0,0 +1,8 @@
+public class multidimentionalArray {
+    public static void main(String[] args) {
+        int mullu[][]={{1,2,3,4},{8,3,9,2,1}};
+        for(int i:mullu){
+            System.out.println(mullu[i]);
+        }
+    }
+}
---
+++ randomArray.java
@@ -0,0 +1,9 @@
+public class randomArray {
+    public static void main(String [] args){
+        int [] num =[n];
+        for (int i=0;i<n;i++){
+            num[i]=Math.random();
+        }
+
+    }
+}
