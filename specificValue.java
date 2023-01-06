2556e11f48117ca9f65baff973668ecb817d9c4c
Abhinendra Singh <abhinendrayadav3@gmail.com>
2023-01-06 23:49:55 +0530

    Update specificValue.java

---
+++ specificValue.java
@@ -0,0 +1,14 @@
+public class specificValue {
+    public static void main(String [] args){
+        int arr[]={1,423,564,64,22};
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==1){
+                System.out.println("Yes its there");
+            }
+            else{
+                System.out.println("no nigga its not here");
+            }
+            break;
+        }
+    }
+}
