fac8ff4e74a71c4eb0727ea19d91d50bef4d957d
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2023-01-17 23:35:23 +0530

    Update sumValueArray.java

--- sumValueArray.java
+++ sumValueArray.java
@@ -3,7 +3,7 @@ public class sumValueArray {
         int[] val={6754,23,645,23,12,75,12};
         int count=0;
         for(int i=0;i<val.length;i++){
-            count=count+val[i];
+            count=count+val[i]; 
             System.out.println(count);
         }
     }
